9d9795348593c4f4949928eb89540d71d88780fe
leonardo <leocode1079@gmail.com>
2026-03-28 21:49:59 +0800

    feat: create route for update password

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,13 +1,14 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.auth import get_current_user
-from app.schemas.user import User
+from app.schemas.user import User, UserInDB
 from sqlalchemy.orm import Session
 from app.repositories import user_repository
 from app.database import get_db
-from app.schemas.user import UserResponse
+from app.schemas.user import UserResponse, PasswordChange
+from app.core.security import verify_password
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -23,3 +24,34 @@ async def read_users_items(current_user: Annotated[User, Depends(get_current_use
 @router.get("/", response_model=list[UserResponse])
 async def get_all_users(current_user: Annotated[User, Depends(get_current_user)], db: Session = Depends(get_db)):
     return user_repository.get(db)
+
+@router.post("/change-password", response_model=UserResponse)
+async def change_password(
+    schema: PasswordChange,
+    current_user: Annotated[UserInDB, Depends(get_current_user)],
+    db: Annotated[Session, Depends(get_db)]
+):
+    # Verifiy old password
+    if not verify_password(schema.old_password, current_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Old password is incorrect'
+        )
+    
+    # Verify password confirmation
+    if not schema.new_password == schema.confirm_password:
+          raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Password confirmation failed'
+        )      
+    
+    # Prevent set the same password
+    if schema.old_password == schema.new_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='New password must be different'
+        )
+    
+    updated_password_user = user_repository.update_password(db, user=current_user, new_password=schema.new_password)
+
+    return updated_password_user
