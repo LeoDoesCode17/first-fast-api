9d670957354afeb1fcad6c42e5fb80b13b56f8a3
leonardo <leocode1079@gmail.com>
2026-03-30 16:43:15 +0800

    feat: define schmas for image response and create

---
+++ app/schemas/image.py
@@ -0,0 +1,12 @@
+# app/schemas/image.py
+from pydantic import BaseModel
+
+class ImageResponse(BaseModel):
+    id: int
+    name: str
+    url: str
+    is_deleted: bool
+
+class ImageCreate(BaseModel):
+    name: str
+    url: str
