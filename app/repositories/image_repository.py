a782e88ba642748557e10c0b5eb2ff7a05db3a79
leonardo <leocode1079@gmail.com>
2026-03-30 17:08:00 +0800

    feat: create a function to update image

--- app/repositories/image_repository.py
+++ app/repositories/image_repository.py
@@ -1,7 +1,7 @@
 # /app/repositories/image_repositories.py
 from sqlalchemy.orm import Session
 from app.tables import Image
-from app.schemas.image import ImageCreate
+from app.schemas.image import ImageCreate, ImageUpdate
 
 def get(db: Session):
     return db.query(Image).filter(Image.is_deleted == False).all()
@@ -24,6 +24,16 @@ def delete(db: Session, id: int):
     if not image_instance:
         return None
     image_instance.is_deleted = True
+    db.commit()
+    db.refresh(image_instance)
+    return image_instance
+
+def update(db: Session, id: int, data: ImageUpdate):
+    image_instance = db.query(Image).filter(Image.id == id, Image.is_deleted == False).first()
+    if not image_instance:
+        return None
+    for key, val in data.model_dump().items():
+        setattr(image_instance, key, val)
     db.commit()
     db.refresh(image_instance)
     return image_instance
