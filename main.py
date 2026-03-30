95ad5053d178fdb1bda625185f0b659d94a2edc9
leonardo <leocode1079@gmail.com>
2026-03-30 14:35:05 +0800

    feat: calls tech router

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routes import auth, users
+from app.routes import auth, users, techs
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(techs.router)
