025c2e6a72ca675b8a8e8c38fc572a686e416264
leonardo <leocode1079@gmail.com>
2026-03-30 14:34:31 +0800

    feat: create function to fetch all techs and create a fetch

---
+++ app/repositories/tech_repository.py
@@ -0,0 +1,14 @@
+# /app/repositories/tech_repositories.py
+from sqlalchemy.orm import Session
+from app.tables import Tech
+from app.schemas.tech import TechCreate
+
+def get(db: Session):
+    return db.query(Tech).all()
+
+def create(db: Session, data: TechCreate):
+    tech_instance = Tech(**data.model_dump())
+    db.add(tech_instance)
+    db.commit()
+    db.refresh(tech_instance)
+    return tech_instance
