cb9f105e616d6d81828fe9e9002640522cde1ceb
leonardo <leocode1079@gmail.com>
2026-03-31 17:05:46 +0800

    feat: define post table structure

--- app/tables.py
+++ app/tables.py
@@ -1,7 +1,8 @@
 # /app/tables.py
 from app.database import Base
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import Integer, String, Boolean, Text
 from sqlalchemy.orm import mapped_column, Mapped
+from app.models.mixins import TimestampMixin
 
 class User(Base):
     __tablename__ = 'users'
@@ -26,3 +27,14 @@ class Image(Base):
     url: Mapped[str] = mapped_column(String, nullable=False)
     alt_text: Mapped[str]  = mapped_column(String, nullable=False)
     is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+class Post(Base, TimestampMixin):
+    __tablename__ = 'posts'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    slug: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    summary: Mapped[str] = mapped_column(Text, nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    is_published: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+
