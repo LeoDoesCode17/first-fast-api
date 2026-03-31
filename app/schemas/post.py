1e935f576056b8484ba98bbcefd162284d5a8732
leonardo <leocode1079@gmail.com>
2026-03-31 17:06:30 +0800

    feat: define schema for post response and create

---
+++ app/schemas/post.py
@@ -0,0 +1,19 @@
+# app/schemas/post.py
+from pydantic import BaseModel
+from datetime import datetime
+
+class PostResponse(BaseModel):
+    id: int
+    title: str
+    slug: str
+    summary: str
+    content: str
+    is_deleted: bool
+    is_published: bool
+    created_at: datetime
+    updated_at: datetime
+
+class PostCreate(BaseModel):
+    title: str
+    summary: str
+    content: str
